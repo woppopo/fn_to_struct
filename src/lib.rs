af69cc973f5a6f36ef2ba5bf6296bdbb29fbc286
woppopo <woppopo@protonmail.com>
2022-11-21 06:20:14 +0900

    Support where clauses (partial)

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,23 @@
 #![feature(fn_traits)]
 #![feature(unboxed_closures)]
 
-pub use paste::paste;
-
 #[macro_export]
 macro_rules! fn2ty {
     () => {};
-    ($vi:vis fn $name:ident<$($life:lifetime,)* $($t:ident $(: $bound:path)?),+>($($arg:ident: $argt:ty),*) -> $ret:ty $body:block $($rem:tt)*) => {
+    ($vi:vis fn $name:ident<$($life:lifetime,)* $($t:ident $(: $bound:path)?),*>($($arg:ident: $argt:ty),*) -> $ret:ty
+    $(where
+        $($wlife:lifetime),*
+        $($wt:ident $(: $wbound:path)?,)*)?
+    $body:block
+    $($rem:tt)*) => {
         #[allow(non_camel_case_types)]
         #[derive(PartialEq, Eq, Clone, Copy, Hash, Debug)]
-        $vi enum $name<$($life,)* $($t $(: $bound)?,)*> {
-            __Generics(core::marker::PhantomData<($(&$life (),)* $($t,)*)>),
+        $vi enum $name<$($life,)* $($t $(: $bound)?,)*> $(where $($wlife,)* $($wt $(: $wbound)?,)*)? {
+            __Generics(::core::marker::PhantomData<($(&$life (),)* $($t,)*)>),
             $name,
         }
 
-        $crate::paste! {
+        ::paste::paste! {
             mod [<__ $name>] {
                 pub(super) use super::$name::$name;
             }
@@ -24,13 +27,17 @@ macro_rules! fn2ty {
             $vi use [<__ $name>]::*;
         }
 
-        impl<$($life,)* $($t $(: $bound)?,)*> Default for $name<$($life,)* $($t,)*> {
+        impl<$($life,)* $($t $(: $bound)?,)*> Default for $name<$($life,)* $($t,)*>
+        $(where $($wlife,)* $($wt $(: $wbound)?,)*)?
+        {
             fn default() -> Self {
                 Self::$name
             }
         }
 
-        impl<$($life,)* $($t $(: $bound)?,)*> core::ops::FnOnce<($($argt,)*)> for $name<$($life,)* $($t,)*> {
+        impl<$($life,)* $($t $(: $bound)?,)*> ::core::ops::FnOnce<($($argt,)*)> for $name<$($life,)* $($t,)*>
+        $(where $($wlife,)* $($wt $(: $wbound)?,)*)?
+        {
             type Output = $ret;
 
             extern "rust-call" fn call_once(self, ($($arg,)*): ($($argt,)*)) -> Self::Output {
@@ -38,13 +45,17 @@ macro_rules! fn2ty {
             }
         }
 
-        impl<$($life,)* $($t $(: $bound)?,)*> core::ops::FnMut<($($argt,)*)> for $name<$($life,)* $($t,)*> {
+        impl<$($life,)* $($t $(: $bound)?,)*> ::core::ops::FnMut<($($argt,)*)> for $name<$($life,)* $($t,)*>
+        $(where $($wlife,)* $($wt $(: $wbound)?,)*)?
+        {
             extern "rust-call" fn call_mut(&mut self, ($($arg,)*): ($($argt,)*)) -> Self::Output {
                 $body
             }
         }
 
-        impl<$($life,)* $($t $(: $bound)?,)*> core::ops::Fn<($($argt,)*)> for $name<$($life,)* $($t,)*> {
+        impl<$($life,)* $($t $(: $bound)?,)*> ::core::ops::Fn<($($argt,)*)> for $name<$($life,)* $($t,)*>
+        $(where $($wlife,)* $($wt $(: $wbound)?,)*)?
+        {
             extern "rust-call" fn call(&self, ($($arg,)*): ($($argt,)*)) -> Self::Output {
                 $body
             }
@@ -52,12 +63,15 @@ macro_rules! fn2ty {
 
         $crate::fn2ty! { $($rem)* }
     };
+    ($vi:vis fn $name:ident<$($life:lifetime),*> $($rem:tt)*) => {
+        $crate::fn2ty!($vi fn $name<$($life,)*> $($rem)*)
+    };
     ($vi:vis fn $name:ident($($arg:ident: $argt:ty),*) -> $ret:ty $body:block $($rem:tt)*) => {
         #[allow(non_camel_case_types)]
         #[derive(PartialEq, Eq, Clone, Copy, Hash, Default, Debug)]
         $vi struct $name;
 
-        impl core::ops::FnOnce<($($argt,)*)> for $name {
+        impl ::core::ops::FnOnce<($($argt,)*)> for $name {
             type Output = $ret;
 
             extern "rust-call" fn call_once(self, ($($arg,)*): ($($argt,)*)) -> Self::Output {
@@ -65,13 +79,13 @@ macro_rules! fn2ty {
             }
         }
 
-        impl core::ops::FnMut<($($argt,)*)> for $name {
+        impl ::core::ops::FnMut<($($argt,)*)> for $name {
             extern "rust-call" fn call_mut(&mut self, ($($arg,)*): ($($argt,)*)) -> Self::Output {
                 $body
             }
         }
 
-        impl core::ops::Fn<($($argt,)*)> for $name {
+        impl ::core::ops::Fn<($($argt,)*)> for $name {
             extern "rust-call" fn call(&self, ($($arg,)*): ($($argt,)*)) -> Self::Output {
                 $body
             }
@@ -96,7 +110,10 @@ fn2ty!(
     pub fn test4() -> test3 {
         test3
     }
-    fn test5<'a, A>(a: A) -> A {
+    fn test5<'a, A>(a: A) -> A
+    where
+        A: Sized,
+    {
         a
     }
 );
